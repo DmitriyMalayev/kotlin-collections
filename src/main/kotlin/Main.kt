847ef45d390568049784693a9006586558dfae93
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-10-18 00:16:37 -0400

    Collections and Lists

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,51 @@
 fun main() {
-    val ints = arrayOf(1, 2, 3, 4, null)
+    val nullableInts = arrayOf(1, 2, 3, 4, null)
+    val arrayOfNonNullInts = arrayOf(1, 2, 3, 4, 5)
+
 
     val nulls = arrayOfNulls<Int>(5)
+    //arrayOf(null, null, null, null, null)  Can support nulls or ints in the future.
 
     val custom = Array(5) { index -> index }
+    //initialization lambda
 
-    val sizeOfArray = ints.size
+    val sizeOfArray = nullableInts.size
 
-    val firstValue = ints[0]
-    ints.set(0, 99)
-    ints[0] = 99
+    //Syntactic sugar
+    val firstValue = nullableInts[0]
+    val alsoFirstValue = nullableInts.get(0)
 
+    nullableInts.set(0, 99)
+    nullableInts[0] = 99
+
+    //Saves memory
     val primitiveInts = intArrayOf(1, 2, 3, 4, 5)
     val primitiveUShorts = ushortArrayOf(1u, 2u, 3u, 4u, 5u)
 
-    //    for (i in 0 until ints.size) {
-    //        println(ints[i])
-    //    }
-    //
-    //    for (element in ints) {
-    //        println(element)
-    //    }
-    //
-    //    ints.forEach { element -> println(element) }
+    for (i in 0 until nullableInts.size) {
+        println(nullableInts[i])
+    }
+
+    for (element in nullableInts) {
+        println(element)
+    }
+
+    //lambda
+    nullableInts.forEach { element -> println(element) }
+    nullableInts.forEach { println(it) }
+
 
     greetThings("Hey", arrayOf("Kotlin", "Programming"))
-    greetThings("Hello", "Kotlin", "Programming", "LinkedIn")
+    greetThingsSimplified("Hello", "Kotlin", "Programming", "LinkedIn")
 }
 
-fun greetThings(greeting: String, vararg items: Any) {
+//requires a new instance of an array every time we call a function
+fun greetThings(greeting: String, items: Array<Any>) {
+    for (item in items) {
+        println("$greeting $item")
+    }
+}
+fun greetThingsSimplified(greeting: String, vararg items: Any) {
     for (item in items) {
         println("$greeting $item")
     }
